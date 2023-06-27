5d83c1bc6d3ee9d798690ab28647d3e3be320513
fusiik <on.kraus@seznam.cz>
2023-06-27 18:05:28 +0200

    lmao

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
+const zero = $("#zero");
 const numberOne = $("#one");
 const numberTwo = $("#two");
 const numberThree = $("#three");
+const numberFour = $("#four");
+const numberFive = $("#five");
+const numberSix = $("#six");
+const numberSeven = $("#seven");
+const numberEight = $("#eight");
+const numberNine = $("#nine");
 const addition = $("#addition");
 const equal = $("#equals");
 const displayNumber = $(".number-display-some");
+const subtraction = $("#substracion");
+const clear = $("#clear");
+const decimal = $("#decimal")
 
-let currentNumber = ""; // Define currentNumber outside the click event handlers
-let a = ""; // Define a outside the addition click event handler
+let currentNumber = "";
+let result = 0;
 
-numberOne.click(function(e) {
-  e.preventDefault();
-  currentNumber += "1"; // Use += to concatenate the value
-  displayNumber.text(currentNumber);
-  console.log(currentNumber);
+const numberButtons = [
+  zero, numberOne, numberTwo, numberThree,
+  numberFour, numberFive, numberSix,
+  numberSeven, numberEight, numberNine
+];
+
+numberButtons.forEach((button, index) => {
+    button.click(function(e) {
+      e.preventDefault();
+      currentNumber += index.toString();
+      displayNumber.text(currentNumber);
+    });
+  });
+
+  equal.click(function(e) {
+    e.preventDefault();
+
+  });
+  
+  addition.click(function(e) {
+    e.preventDefault();
+    result = Number(currentNumber) + Number(result)
+    displayNumber.text(result)
+    currentNumber = ""
+  });
+  
+  subtraction.click(function(e) {
+    e.preventDefault();
+    result = currentNumber
+    if (result !== "") {
+        result = (Number(result) - Number(currentNumber))
+        displayNumber.text(result)
+    }
+    currentNumber = ""
+  });
+
+decimal.click(function (e) { 
+    e.preventDefault();
+    console.log(currentNumber);
 });
 
-numberTwo.click(function(e) {
-  e.preventDefault();
-  currentNumber += "2";
-  displayNumber.text(currentNumber);
-  console.log(currentNumber);
-});
-
-numberThree.click(function(e) {
-  e.preventDefault();
-  currentNumber += "3";
-  displayNumber.text(currentNumber);
-  console.log(currentNumber);
-});
-
-addition.click(function(e) {
-  e.preventDefault();
-  a = currentNumber; // Assign the value of currentNumber to a
-  currentNumber = ""; // Reset currentNumber for the next input
-  displayNumber.empty();
-});
-
-equal.click(function(e) {
-  e.preventDefault();
-  if (a !== "" && currentNumber !== "") {
-    let b = Number(a) + Number(currentNumber); // Convert strings to numbers for addition
-    displayNumber.text(b);
-    currentNumber = ""; // Reset currentNumber after performing addition
-    a = ""; // Reset a for the next calculation
-  }
+clear.click(function (e) { 
+    e.preventDefault();
+    currentNumber = "";
+    result = 0;
+    displayNumber.text("0");
 });
