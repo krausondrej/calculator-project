376a2c6417eeb80052baa8e9b18c45eef5934608
fusiik <on.kraus@seznam.cz>
2023-06-28 18:32:28 +0200

    lmao

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const numberEight = $("#eight");
 const numberNine = $("#nine");
 const addition = $("#addition");
 const equal = $("#equals");
-const displayNumber = $(".number-display-some");
 const subtraction = $("#substracion");
 const clear = $("#clear");
 const decimal = $("#decimal")
-
-let currentNumber = "";
-let result = 0;
+const percent = $("#percent")
+const multiplication = $("#multiplication")
+const division = $("#division")
+const displayNumber = $(".number-display-some");
 
 const numberButtons = [
   zero, numberOne, numberTwo, numberThree,
@@ -24,44 +24,109 @@ const numberButtons = [
   numberSeven, numberEight, numberNine
 ];
 
+let operator = "";
+let result = 0;
+let currentNumber = 0;
+
 numberButtons.forEach((button, index) => {
-    button.click(function(e) {
-      e.preventDefault();
-      currentNumber += index.toString();
+  button.click(function(e) {
+    e.preventDefault();
+
+    if (currentNumber < 100000000) {
+      currentNumber = currentNumber * 10 + index;
       displayNumber.text(currentNumber);
-    });
-  });
 
-  equal.click(function(e) {
-    e.preventDefault();
-
-  });
-  
-  addition.click(function(e) {
-    e.preventDefault();
-    result = Number(currentNumber) + Number(result)
-    displayNumber.text(result)
-    currentNumber = ""
-  });
-  
-  subtraction.click(function(e) {
-    e.preventDefault();
-    result = currentNumber
-    if (result !== "") {
-        result = (Number(result) - Number(currentNumber))
-        displayNumber.text(result)
+      if (currentNumber > 999999) {
+        displayNumber.css('font-size', '60px');
+      }
     }
-    currentNumber = ""
   });
-
-decimal.click(function (e) { 
-    e.preventDefault();
-    console.log(currentNumber);
 });
+
+equal.click(function(e) {
+  e.preventDefault();
+  equalCalcualtions();
+  displayNumber.text(result);
+  currentNumber = 0;
+  operator = "";
+});
+
+addition.click(function(e) {
+  e.preventDefault();
+  performCalculation("+");
+});
+
+subtraction.click(function(e) {
+  e.preventDefault();
+  performCalculation("-");
+});
+
+multiplication.click(function(e) {
+  e.preventDefault();
+  performCalculation("x");
+});
+
+division.click(function(e) {
+  e.preventDefault();
+  performCalculation("/");
+});
+
+function performCalculation(op) {
+  counterSome();
+  currentNumber = 0;
+  operator = op;
+}
+
+function counterSome() {
+  if (result !== 0 && currentNumber !== 0) {
+    if (operator === "+") {
+      result += currentNumber;
+    } else if (operator === "-") {
+      result -= currentNumber;
+    } else if (operator === "x") {
+      result *= currentNumber;
+    } else if (operator === "/") {
+      result /= currentNumber;
+    }
+    displayNumber.text(result);
+  } else if (result === 0) {
+    result = currentNumber;
+    displayNumber.text(result);
+  }
+}
+
+function equalCalcualtions() {
+  if (operator && currentNumber !== 0) {
+    if (operator === "+") {
+      result += currentNumber;
+    } else if (operator === "-") {
+      result -= currentNumber;
+    } else if (operator === "x") {
+      result *= currentNumber;
+    } else if (operator === "/") {
+      result /= currentNumber;
+    }
+  } else if (result === 0) {
+    result = currentNumber;
+  }
+}
 
 clear.click(function (e) { 
     e.preventDefault();
-    currentNumber = "";
+    currentNumber = 0;
     result = 0;
+    operator = "";
     displayNumber.text("0");
+});
+
+decimal.click(function (e) {
+  e.preventDefault();
+  
+
+});
+
+percent.click(function (e) {
+  e.preventDefault();
+  console.log(result + " vysledek");
+  console.log(currentNumber + " psane");
 });
